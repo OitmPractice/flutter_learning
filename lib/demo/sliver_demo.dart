1c4271b5fbdbb458eee615f7022e14b96c8c0d9a
songshuran <happy_ran@outlook.com>
2019-03-15 17:00:29 +0800

    ignore

---
+++ lib/demo/sliver_demo.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import '../model/post.dart';
+
+class SliverDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            // title: Text('Oitm'),
+            // pinned: true,
+            floating: true,
+            expandedHeight: 178,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Oitm Flutter'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3.0,
+                ),
+              ),
+              background: Image.network(
+                'https://resources.ninghao.net/images/overkill.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverSafeArea(
+            sliver: SliverPadding(
+              sliver: SliverGridDemo(),
+              padding: EdgeInsets.all(8),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SliverListDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: 32.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(12.0),
+              elevation: 14.0,
+              shadowColor: Colors.grey.withOpacity(0.5),
+              child: Stack(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Image.network(
+                      posts[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    top: 32.0,
+                    left: 32.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          posts[index].title,
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        ),
+                        Text(
+                          posts[index].author,
+                          style: TextStyle(fontSize: 13.0, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        childCount: posts.length,
+      ),
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+          childAspectRatio: 1.0),
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Container(
+            child: Image.network(
+              posts[index].imageUrl,
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        childCount: posts.length,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './demo/bottom_navigation_bar_demo.dart';
 import './demo/basic_demo.dart';
 import './demo/layout_demo.dart';
 import './demo/view_demo.dart';
+import './demo/sliver_demo.dart';
 
 void main() => runApp(App());
 
@@ -18,11 +19,12 @@ class App extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
        home: Home(),
-       theme: ThemeData(
-         primarySwatch: Colors.yellow,
-         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
-         splashColor: Colors.white70
-       ),
+      // home: SliverDemo(),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70
+      ),
     );
   }
 }
@@ -71,7 +73,8 @@ class Home extends StatelessWidget {
           ListViewDemo(),
           BasicDemo(),
           LayoutDemo(),
-          ViewDemo(),
+          // ViewDemo(),
+          SliverDemo(),
         ],
       ),
       drawer: DrawerDemo(),
