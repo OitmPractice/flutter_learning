1e8cdce29fdb59d955a73e6050071fcda8d6df4b
songshuran <happy_ran@outlook.com>
2019-03-28 11:41:02 +0800

    form demo finish

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -5,22 +5,115 @@ class FormDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Theme(
-        //ThemeData 会完全取代之前设定的主题
-        // data: ThemeData(
-        //   primaryColor: Colors.black
-        // ),
-        data: Theme.of(context).copyWith(
-          primaryColor: Colors.black
-        ),
-        child: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFieldDemo(),
-            ],
-          ),
+          //ThemeData 会完全取代之前设定的主题
+          // data: ThemeData(
+          //   primaryColor: Colors.black
+          // ),
+          data: Theme.of(context).copyWith(primaryColor: Colors.black),
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // TextFieldDemo(),
+                RegisterForm(),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+//如何获取表单里面的数据， 添加GlobalKey，交给表单，使用key引用表单
+  final registerFormKey = GlobalKey<FormState>();
+
+  String username;
+  String password;
+
+  bool autovalidate = false;
+
+  void submitRegisterForm() {
+    if (registerFormKey.currentState.validate()) {
+      
+      registerFormKey.currentState.save();
+      debugPrint('username: $username ， password: $password');
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Registering'),
         )
+      );
+
+    }else {
+      setState(() {
+        autovalidate = true;
+      });
+    }
+   
+  }
+
+  String validateUsername(value) {
+    if (value.isEmpty) {
+      return 'Username is required';
+    }
+    return null;
+  }
+
+  String validatePassword(value) {
+    if (value.isEmpty) {
+      return 'Password is required';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: registerFormKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            onSaved: (value) {
+              username = value; 
+            },
+            decoration: InputDecoration(
+              labelText: 'Username',
+              helperText: '',
+            ),
+            validator: validateUsername,
+            autovalidate: autovalidate,
+          ),
+          TextFormField(
+            onSaved: (value) {
+              password = value;
+            },
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              helperText: '',
+            ),
+            validator: validatePassword,
+            autovalidate: autovalidate,
+          ),
+          SizedBox(
+            height: 32.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('Register', style: TextStyle(color: Colors.white)),
+              elevation: 0.0,
+              onPressed: submitRegisterForm,
+            ),
+          )
+        ],
       ),
     );
   }
@@ -32,10 +125,39 @@ class TextFieldDemo extends StatefulWidget {
 }
 
 class _TextFieldDemoState extends State<TextFieldDemo> {
+  final textEditingController = TextEditingController();
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  void initState() {
+    super.initState();
+    // textEditingController.text = 'hello world';
+    textEditingController.addListener(() {
+      debugPrint('input: ${textEditingController.text}');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextField(
-      
+      controller: textEditingController,
+      // onChanged: (value){
+      //   debugPrint('input: $value');
+      // },
+      onSubmitted: (value) {
+        debugPrint('submit: $value');
+      },
+      decoration: InputDecoration(
+        icon: Icon(Icons.subject),
+        labelText: 'Title',
+        hintText: 'Enter the post title',
+        // border: InputBorder.none,
+        // border: OutlineInputBorder(),
+        filled: true,
+      ),
     );
   }
 }
